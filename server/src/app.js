83b54729ec2d17b219644834e5acba197a40f39c
hampuslundblad <hampus.lundblad@hotmail.se>
2022-08-11 20:09:14 +0200

    added controllers, models, policies, routes, install bunch of modules

---
+++ server/src/app.js
@@ -0,0 +1,19 @@
+console.log("Hello");
+const express = require("express");
+const bodyParser = require("body-parser");
+const cors = require("cors");
+const morgan = require("morgan");
+const { sequelize } = require("./models");
+const config = require("./config/config");
+
+const app = express();
+app.use(morgan("combine"));
+app.use(bodyParser.json());
+app.use(cors());
+
+require("./routes")(app);
+
+sequelize.sync().then(() => {
+  app.listen(config.port);
+  console.log(`Server started, ${config.port}`);
+});
---
+++ server/src/models/User.js
@@ -0,0 +1,40 @@
+const Promise = require("bluebird");
+const bcrypt = Promise.promisifyAll(require("bcryptjs"));
+
+function hashPassword(user, options) {
+  const SALT_FACTOR = 8;
+
+  if (!user.changed("password")) {
+    return;
+  }
+  return bcrypt
+    .genSalt(SALT_FACTOR)
+    .then((salt) => bcrypt.hash(user.password, salt, null))
+    .then((hash) => {
+      user.setDataValue("password", hash);
+    });
+}
+
+module.exports = (sequelize, DataTypes) => {
+  const User = sequelize.define(
+    "User",
+    {
+      email: {
+        type: DataTypes.STRING,
+        unique: true,
+      },
+      password: {
+        type: DataTypes.STRING,
+      },
+    },
+    {
+      hooks: {
+        beforeSave: hashPassword,
+      },
+    }
+  );
+  User.prototype.comparePassword = function (password) {
+    return bcrypt.compare(password, this.password);
+  };
+  return User;
+};
