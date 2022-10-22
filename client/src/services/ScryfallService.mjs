5c91458c77c1a050c9cf935ead8a13e870cff47c
hampuslundblad <hampus.lundblad@hotmail.se>
2022-10-22 16:07:43 +0200

    added typescript to scyfall collection, refactor to cardsearch button popup

--- client/src/services/ScryfallService.mjs
+++
@@ -1,7 +0,0 @@
-import { Scryfall } from "@/services/Api";
-
-export default {
-  searchCard(query) {
-    return Scryfall().get(`/cards/search?q=${query}`);
-  },
-};
---
+++ client/src/services/models/Requests/ScryfallGetCardRequest.ts
@@ -0,0 +1,7 @@
+type ScryfallGetCardRequest = {
+  name: string;
+  priceEur: string;
+  foil: boolean;
+  imageUri: string;
+  set: string;
+};
