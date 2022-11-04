c36498cee4477b64c998b381fe2d178de225ea01
hampuslundblad <hampus.lundblad@teliacompany.com>
2022-11-04 15:20:11 +0100

    minor refactoring to backend

--- server/src/controllers/CollectionController.js
+++ server/src/controllers/CollectionController.js
@@ -3,25 +3,8 @@ const { User, Collection, Card } = require("../database");
 module.exports = {
   async addCardToCollection(req, res) {
     try {
-      const userId = req.body.userId;
-      console.log("userID", userId);
-
-      const collectionName = req.body.collectionName;
-      const collection = await Collection.findOne({
-        where: {
-          name: collectionName,
-          UserId: userId,
-        },
-      });
-      const card = await Card.create({
-        CollectionId: collection.id,
-        name: req.body.card.name,
-        price: req.body.card.price,
-        set: req.body.card.set,
-        quantity: req.body.card.quantity,
-        foil: req.body.card.foil,
-        imageUrl: req.body.card.imageUrl,
-      });
+      const collection = findOneCollection(req);
+      const card = createCard(req);
       return res.send({ message: "Successfully added card to the collection" });
     } catch (err) {
       console.log(err);
@@ -33,15 +16,7 @@ module.exports = {
   },
   async findAll(req, res) {
     try {
-      const userId = req.params.userId;
-      const userCollections = await Collection.findAll({
-        where: {
-          userId: userId,
-        },
-        include: {
-          model: Card,
-        },
-      });
+      const userCollections = findAllCollections(req);
       const userCollectionsJson = JSON.stringify(userCollections);
       res.send({
         userCollections: userCollectionsJson,
@@ -53,4 +28,39 @@ module.exports = {
       });
     }
   },
+};
+const createCard = async (req) => {
+  const card = await Card.create({
+    CollectionId: collection.id,
+    name: req.body.card.name,
+    price: req.body.card.price,
+    set: req.body.card.set,
+    quantity: req.body.card.quantity,
+    foil: req.body.card.foil,
+    imageUrl: req.body.card.imageUrl,
+  });
+  return card;
+};
+const findOneCollection = async (req) => {
+  const userId = req.body.userId;
+  const collectionName = req.body.collectionName;
+  const collection = await Collection.findOne({
+    where: {
+      name: collectionName,
+      UserId: userId,
+    },
+  });
+  return collection;
+};
+const findAllCollections = async (req) => {
+  const userId = req.params.userId;
+  const userCollections = await Collection.findAll({
+    where: {
+      userId: userId,
+    },
+    include: {
+      model: Card,
+    },
+  });
+  return userCollections;
 };
